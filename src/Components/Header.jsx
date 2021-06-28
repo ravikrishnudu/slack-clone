ec1489b27bdb105fe61662b99d82f603eeca93d3
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-06-28 15:03:18 +0530

    Header ui

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerLeft}>
+        <Avatar
+          className={styles.avatar}
+          //   alt={user?.displayName}
+          //   src={user?.photoURL}
+          alt="ravi"
+          src=""
+        />
+        <AccessTimeIcon />
+      </div>
+      <div className={styles.search}>
+        <SearchIcon />
+        <input type="text" placeholder="welcome to slack clone " />
+      </div>
+      <div className={styles.headerRight}>
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
