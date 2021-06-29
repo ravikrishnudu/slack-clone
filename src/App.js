ceda2cd752fbb24859dd9855427811bdcfdbfba2
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-06-29 09:04:52 +0530

    sidebar with D

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import Header from "./Components/Header";
 import styles from "./App.module.css";
 import Sidebar from "./Components/Sidebar";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./Components/Chat";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className={styles.body}>
-        <Sidebar />
-      </div>
+    <div>
+      <Router>
+        <Header />
+        <div className={styles.appBody}>
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>weelcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
