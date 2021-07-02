8da637c242605a745dd7dc305c2b3419030db7ed
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-07-02 23:40:21 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import styles from "./App.module.css";
 import Sidebar from "./Components/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Components/Chat";
+import Login from "./Components/Login";
+
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div>
       <Router>
-        <Header />
-        <div className={styles.appBody}>
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>weelcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className={styles.appBody}>
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>weelcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/Components/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./Login.module.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./Firebase";
+
+function Login() { 
+    
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className={styles.login}>
+      <div className={styles.container}>
+        {/* <img
+          src="https://a.slack-edge.com/bv1-9/slack_logo-ebd02d1.svg"
+          alt=""
+        /> */}
+
+        <svg
+          className={styles.logo}
+          enable-background="new 0 0 2447.6 2452.5"
+          viewBox="0 0 2447.6 2452.5"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-rule="evenodd" fill-rule="evenodd">
+            <path
+              d="m897.4 0c-135.3.1-244.8 109.9-244.7 245.2-.1 135.3 109.5 245.1 244.8 245.2h244.8v-245.1c.1-135.3-109.5-245.1-244.9-245.3.1 0 .1 0 0 0m0 654h-652.6c-135.3.1-244.9 109.9-244.8 245.2-.2 135.3 109.4 245.1 244.7 245.3h652.7c135.3-.1 244.9-109.9 244.8-245.2.1-135.4-109.5-245.2-244.8-245.3z"
+              fill="#36c5f0"
+            />
+            <path
+              d="m2447.6 899.2c.1-135.3-109.5-245.1-244.8-245.2-135.3.1-244.9 109.9-244.8 245.2v245.3h244.8c135.3-.1 244.9-109.9 244.8-245.3zm-652.7 0v-654c.1-135.2-109.4-245-244.7-245.2-135.3.1-244.9 109.9-244.8 245.2v654c-.2 135.3 109.4 245.1 244.7 245.3 135.3-.1 244.9-109.9 244.8-245.3z"
+              fill="#2eb67d"
+            />
+            <path
+              d="m1550.1 2452.5c135.3-.1 244.9-109.9 244.8-245.2.1-135.3-109.5-245.1-244.8-245.2h-244.8v245.2c-.1 135.2 109.5 245 244.8 245.2zm0-654.1h652.7c135.3-.1 244.9-109.9 244.8-245.2.2-135.3-109.4-245.1-244.7-245.3h-652.7c-135.3.1-244.9 109.9-244.8 245.2-.1 135.4 109.4 245.2 244.7 245.3z"
+              fill="#ecb22e"
+            />
+            <path
+              d="m0 1553.2c-.1 135.3 109.5 245.1 244.8 245.2 135.3-.1 244.9-109.9 244.8-245.2v-245.2h-244.8c-135.3.1-244.9 109.9-244.8 245.2zm652.7 0v654c-.2 135.3 109.4 245.1 244.7 245.3 135.3-.1 244.9-109.9 244.8-245.2v-653.9c.2-135.3-109.4-245.1-244.7-245.3-135.4 0-244.9 109.8-244.8 245.1 0 0 0 .1 0 0"
+              fill="#e01e5a"
+            />
+          </g>
+        </svg>
+
+        <h1>Sign in to slack </h1>
+        <p>slack clone</p>
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
